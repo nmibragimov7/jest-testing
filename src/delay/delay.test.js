3f3e79152c2763a671f9bf1c9250f6b31c1f5528
Nurlan Ibragimov <nibragimov@1cb.kz>
2022-10-05 14:03:36 +0600

    fix: fix

--- src/delay/delay.test.js
+++ src/delay/delay.test.js
@@ -1,6 +1,7 @@
 const delay = require('./delay');
 
 describe('delay', () => {
+
     test('Корректное значение', async () => {
         const sum = await delay(() => (5 + 5), 1000);
         expect(sum).toBe(10);
